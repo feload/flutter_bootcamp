bf696fb6828253ac111a6a051200e76544d069fb
Felipe <feload@gmail.com>
2019-12-04 09:20:13 -0600

    Add tel and email

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -11,38 +13,64 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text("White"),
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage("images/feload.jpg"),
+                  ),
+                  Text(
+                    "Felipe L.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Pacifico"
                     ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.green,
+                  ),
+                  Text(
+                    "Software Architect".toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.teal[50],
+                      fontFamily: "Source Sans Pro"
                     )
-                  ],
-                ),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text("Blue"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    color: Colors.white,
+                    margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.phone, color: Colors.teal[700],),
+                        SizedBox(width: 10.0,),
+                        Text("+52 4611301332",
+                          style: TextStyle(
+                            color: Colors.teal[800]
+                          )
+                        )
+                      ],
+                    )
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                    color: Colors.white,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.email, color: Colors.teal[700],),
+                        SizedBox(width: 10.0,),
+                        Text("feload@gmail.com",
+                          style: TextStyle(
+                            color: Colors.teal[800]),
+                          )
+                      ],
+                    ),
+                  )
+                ],
               )
             ],
           )
